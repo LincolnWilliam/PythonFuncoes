b2d751c5daa2a996447ce4073b30bed6b5a7fee3
William <william@swind.com.br>
2022-05-04 10:17:38 -0300

    Implementando o input

--- Funcao01.py
+++ Funcao01.py
@@ -1,14 +1,21 @@
 # Escreva uma função que recebe dois parâmetros e imprime o menor
 # dos dois. Se eles forem iguais, imprima que eles são iguais.
 def menorNumero (var1,var2):
-     if var1 < var2:
-         print(f'menor numero é: {var1}')
-     elif var1 > var2:
-         print(f'menor numero é:{var2}')
-     else:
-         print('os numeros sao iguais.')
+
+    if var1 < var2:
+         print(f'Entre {var1} e {var2} o menor número é: {var1}')
+    elif var1 > var2:
+         print(f'Entre {var1} e {var2} o menor numero é:{var2}')
+    else:
+         print(f'os numeros {var1} e {var2} são iguais.')
+
+var1 = int(input('Informe o primeiro número: '))
+var2 = int(input('Informe o segundo número: '))
+
+numero = menorNumero(var1,var2)
+
 
 #------------ outra maneira de teste direto --------------------
-menorN(0,5)
-menorN(10,3)
-menorN(42,42)
+# menorNumero(0,5)
+# menorNumero(10,3)
+# menorNumero(42,42)
