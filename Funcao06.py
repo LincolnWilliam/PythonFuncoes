ccb560433ed4a88c5fc9c11f63c0a8432d7e6828
William <william@swind.com.br>
2022-05-05 17:00:30 -0300

    calculando aumento de salário em %

---
+++ Funcao06.py
@@ -0,0 +1,13 @@
+# Crie uma função que receba  2 números.
+# O primeiro é um valor e o segundo um percentual (ex. 10%).
+# Retorne o valor do primeiro número somado do aumento do percentual do mesmo.
+
+def aumento_salario(a,b):
+    return a + (a * b / 100)
+
+a = float(input('Informe um valor atual do salário: R$ '))
+b = float(input('Informe o aumento: % '))
+
+i = aumento_salario(a,b)
+print(f'seu salário subiu de R$ {a} para R$ {i:.2f}')
+
