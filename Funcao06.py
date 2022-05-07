d804b7a4245abcdea4ef903c92aff63a01d6bf49
William <william@swind.com.br>
2022-05-06 21:27:21 -0300

    calcular o aumento de salário.

--- Funcao06.py
+++ Funcao06.py
@@ -1,7 +1,8 @@
 # Crie uma função que receba  2 números.
 # O primeiro é um valor e o segundo um percentual (ex. 10%).
 # Retorne o valor do primeiro número somado do aumento do percentual do mesmo.
-# resumo - calcular o aumento de salário.
+# resumo :
+# calcular o aumento de salário.
 
 def aumento_salario(a,b):
     return a + (a * b / 100)
